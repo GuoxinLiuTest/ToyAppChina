5cf69b4cadbe307820879a1866fa5c07442401dc
Guoxin Liu <Guoxin.Liu@software.dell.com>
2015-12-04 10:17:05 +0800

    Initialize the project

---
+++ ToyAppChina/Controllers/StaticPagesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToyAppChina.Models;
+
+namespace ToyAppChina.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+        [Route("")]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [Route("help")]
+        public ActionResult Help()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("contact")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ ToyAppChina/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToyAppChina.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
